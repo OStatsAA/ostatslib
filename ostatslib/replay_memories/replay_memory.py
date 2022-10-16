5820ed68a8595bd67d77babcb2b72e5117abb26b
Guilherme <g.lisboa.oliveira@outlook.com>
2022-10-16 13:50:25 +0000

    feat: adds capacity helper methods to ReplayMemory

--- ostatslib/replay_memories/replay_memory.py
+++ ostatslib/replay_memories/replay_memory.py
@@ -13,12 +13,13 @@ class ReplayMemory:
         next states after action taken and reward.
     """
 
-    def __init__(self, length: int = 10000) -> None:
-        self.__states = np.empty(length, dtype=object)
-        self.__actions = np.empty(length, dtype=object)
-        self.__next_states = np.empty(length, dtype=object)
-        self.__rewards = np.empty(length, dtype=float)
+    def __init__(self, max_length: int = 10000) -> None:
+        self.__states = np.empty(max_length, dtype=object)
+        self.__actions = np.empty(max_length, dtype=object)
+        self.__next_states = np.empty(max_length, dtype=object)
+        self.__rewards = np.empty(max_length, dtype=float)
         self.__next_index = 0
+        self.__max_length = max_length
 
     def append(self,
                state: State,
@@ -61,6 +62,24 @@ class ReplayMemory:
             "actions": self.__actions[:self.__next_index],
             "rewards": self.__rewards[:self.__next_index]
         }
+
+    def get_free_space_count(self) -> int:
+        """
+        Returns number of empty rows
+
+        Returns:
+            int: number of empty row untill memory is full
+        """
+        return self.__max_length - self.__next_index
+
+    def is_full(self) -> bool:
+        """
+        Returns whether the memory is at full capacity or not
+
+        Returns:
+            bool: True if memory if full
+        """
+        return self.__max_length == self.__next_index
 
     def __len__(self):
         return self.__next_index
--- tests/unit_tests/replay_memories/test_replay_memory.py
+++ tests/unit_tests/replay_memories/test_replay_memory.py
@@ -2,6 +2,8 @@
 ReplayMemory testing module
 """
 from random import choice, uniform
+
+import pytest
 from ostatslib.replay_memories import ReplayMemory
 from ostatslib.states.state import State
 
@@ -48,5 +50,34 @@ def test_gets_state_action_reward_dict() -> None:
 
     sar_dict = replay_memory.get_sar_entries()
     assert list(sar_dict.keys()) == ["states", "actions", "rewards"]
-    assert [len(value) == 1000 for value in sar_dict.values()] == [
-        True, True, True]
+    assert [len(value) == 1000 for value in sar_dict.values()] == [True] * 3
+
+
+def test_getting_free_rows_count() -> None:
+    """
+    Memory should have a method to get free rows count
+    """
+    replay_memory = ReplayMemory(100)
+    for _ in range(90):
+        action_name = choice(DUMMY_ACTIONS)
+        reward = uniform(-100, 100)
+        replay_memory.append(State(), action_name, State(), reward)
+
+    free_rows = replay_memory.get_free_space_count()
+    assert free_rows == 10
+
+
+@pytest.mark.parametrize('max_length, used, expected',
+                         [(100, 90, False), (100, 100, True)]
+                         )
+def test_getting_whether_memory_is_full(max_length, used, expected) -> None:
+    """
+    Memory should have a method to get whether it's full or not
+    """
+    replay_memory = ReplayMemory(max_length)
+    for _ in range(used):
+        action_name = choice(DUMMY_ACTIONS)
+        reward = uniform(-100, 100)
+        replay_memory.append(State(), action_name, State(), reward)
+
+    assert replay_memory.is_full() == expected

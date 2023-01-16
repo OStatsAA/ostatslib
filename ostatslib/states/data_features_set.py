42a0e895ad8652bc46ff033f0ac05270109d5910
Guilherme <g.lisboa.oliveira@outlook.com>
2023-01-16 20:35:00 +0000

    feat: adds __sub__ and refator state,featuresset

--- ostatslib/states/data_features_set.py
+++ ostatslib/states/data_features_set.py
@@ -1,23 +1,25 @@
 """
-FeaturesSets classes Module
+DataFeaturesSets module
 """
 
-from dataclasses import astuple, dataclass
+from dataclasses import astuple, dataclass, field
 
 import numpy as np
 
+from ostatslib.states.features_set import FeaturesSet
+
 
 @dataclass(init=False)
-class DataFeaturesSet:
+class DataFeaturesSet(FeaturesSet):
     """
     Class to hold features extracted from a dataset.
     """
-    log_rows_count: float = 0
-    is_response_dichotomous: int = 0
-    is_response_quantitative: int = 0
-    is_response_discrete: int = 0
-    is_response_positive_values_only: int = 0
-    are_linear_model_residuals_correlated: int = 0
+    log_rows_count: float = field(default=0)
+    is_response_dichotomous: int = field(default=0)
+    is_response_quantitative: int = field(default=0)
+    is_response_discrete: int = field(default=0)
+    is_response_positive_values_only: int = field(default=0)
+    are_linear_model_residuals_correlated: int = field(default=0)
 
     def __array__(self):
         return np.array(astuple(self))

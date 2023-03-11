44add5f0fa5afe1407d02c2abce2fe0c2a9027e2
gui-lisboa <47819834+gui-lisboa@users.noreply.github.com>
2023-03-11 03:33:48 +0000

    refactor: removes no longer required code

--- ostatslib/reinforcement_learning_methods/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Reinforcement Learning methods module
-"""
-
-from .reinforcement_learning_method import ReinforcementLearningMethod
-
-from .actor_critic import ActorCritic
-from .support_vector_regression import SupportVectorRegression

0c33459034f33c6ccf7e3f49ae333f6c9127c6b9
gui-lisboa <47819834+gui-lisboa@users.noreply.github.com>
2023-03-24 12:54:10 +0000

    feat: adds args to training script

--- scripts/train_agent.py
+++ scripts/train_agent.py
@@ -1,10 +1,17 @@
+import argparse
 from ostatslib.agents import PPOAgent
 from datetime import datetime
 
 import warnings
 warnings.filterwarnings("ignore")
 
+parser = argparse.ArgumentParser(description="Agent training script")
+parser.add_argument("--steps", nargs='?', const=1, type=int, default=int(250e3))
+parser.add_argument("--env-count", nargs='?', const=1, type=int, default=4)
+parser.add_argument("--name", nargs='?', const=1, type=str, default=f'ppo_agent{datetime.now()}')
 
-agent = PPOAgent(training_envs_count=4)
-agent.train(int(250e3))
-agent.save(f'ppo_agent_{datetime.now()}')
+args = parser.parse_args()
+
+agent = PPOAgent(training_envs_count=int(args.env_count))
+agent.train(int(args.steps))
+agent.save(args.name)

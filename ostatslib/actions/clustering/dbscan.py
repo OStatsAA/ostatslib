763ba0061b8d3e7763e4f9941f4f63616f34c9ee
gui-lisboa <47819834+gui-lisboa@users.noreply.github.com>
2023-06-01 01:28:03 +0000

    feat: add validate_state decorator to clustering actions

--- ostatslib/actions/clustering/dbscan.py
+++ ostatslib/actions/clustering/dbscan.py
@@ -2,20 +2,23 @@
 DBSCAN module
 """
 
+import operator
 from pandas import DataFrame
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import silhouette_score
 
-from ostatslib import config
 from ostatslib.states import State
 from ..action import Action, ActionInfo, ActionResult
 from ..utils import (calculate_score_reward,
                      reward_cap,
-                     update_state_score)
+                     update_state_score,
+                     validate_state)
 
 _ACTION_NAME = "DBSCAN"
+_VALIDATIONS = [('response_variable_label', operator.eq, '')]
 
 
+@validate_state(action_name=_ACTION_NAME, validator_fns=_VALIDATIONS)
 @reward_cap
 def _dbscan(state: State, data: DataFrame) -> ActionResult[DBSCAN]:
     """
@@ -28,12 +31,6 @@ def _dbscan(state: State, data: DataFrame) -> ActionResult[DBSCAN]:
     Returns:
         ActionResult[DBSCAN]: action result
     """
-    if not __is_valid_state(state):
-        return state, config.MIN_REWARD, ActionInfo(action_name=_ACTION_NAME,
-                                                    action_fn=_dbscan,
-                                                    model=None,
-                                                    raised_exception=False)
-
     db_scan = DBSCAN()
     db_scan.fit(data)
 
@@ -45,13 +42,6 @@ def _dbscan(state: State, data: DataFrame) -> ActionResult[DBSCAN]:
                                      action_fn=_dbscan,
                                      model=db_scan,
                                      raised_exception=False)
-
-
-def __is_valid_state(state: State) -> bool:
-    if bool(state.get("clusters_count")) or bool(state.get("response_variable_label")):
-        return False
-
-    return True
 
 
 dbscan: Action[DBSCAN] = _dbscan

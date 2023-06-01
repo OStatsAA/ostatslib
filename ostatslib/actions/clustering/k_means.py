bda59a9225f404311d86d01210f13ebb5583688e
gui-lisboa <47819834+gui-lisboa@users.noreply.github.com>
2023-06-01 01:28:03 +0000

    feat: add validate_state decorator to clustering actions

--- ostatslib/actions/clustering/k_means.py
+++ ostatslib/actions/clustering/k_means.py
@@ -2,20 +2,24 @@
 K-Means module
 """
 
+import operator
 from pandas import DataFrame
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
-from ostatslib import config
 from ostatslib.states import State
 from ..action import Action, ActionInfo, ActionResult
 from ..utils import (calculate_score_reward,
                      reward_cap,
-                     update_state_score)
+                     update_state_score,
+                     validate_state)
 
 _ACTION_NAME = "K-Means"
+_VALIDATIONS = [('response_variable_label', operator.eq, ''),
+                ('clusters_count', operator.gt, 0)]
 
 
+@validate_state(action_name=_ACTION_NAME, validator_fns=_VALIDATIONS)
 @reward_cap
 def _k_means(state: State, data: DataFrame) -> ActionResult[KMeans]:
     """
@@ -28,12 +32,6 @@ def _k_means(state: State, data: DataFrame) -> ActionResult[KMeans]:
     Returns:
         ActionResult[KMeans]: action result
     """
-    if not __is_valid_state(state):
-        return state, config.MIN_REWARD, ActionInfo(action_name=_ACTION_NAME,
-                                                    action_fn=_k_means,
-                                                    model=None,
-                                                    raised_exception=False)
-
     clusters_count: int = state.get("clusters_count")
     kmeans = KMeans(n_clusters=clusters_count)
     kmeans.fit(data)
@@ -46,13 +44,6 @@ def _k_means(state: State, data: DataFrame) -> ActionResult[KMeans]:
                                      action_fn=_k_means,
                                      model=kmeans,
                                      raised_exception=False)
-
-
-def __is_valid_state(state: State) -> bool:
-    if not bool(state.get("clusters_count")) or bool(state.get("response_variable_label")):
-        return False
-
-    return True
 
 
 k_means: Action[KMeans] = _k_means

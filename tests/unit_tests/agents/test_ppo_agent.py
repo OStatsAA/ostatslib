49db3ca60db6c6492cc879715cdb13d7dfd45e62
gui-lisboa <47819834+gui-lisboa@users.noreply.github.com>
2023-06-09 00:27:34 +0000

    feat: add environment kwargs to PPO init

--- tests/unit_tests/agents/test_ppo_agent.py
+++ tests/unit_tests/agents/test_ppo_agent.py
@@ -1,22 +1,11 @@
 from pandas import DataFrame
-from datacooker.recipes import LogitRecipe
-from datacooker.variables import ContinousVariable
-import pytest
 
 from ostatslib.agents.ppo_agent import PPOAgent
+from ostatslib.environments.data_generators import datacooker_generator
 
 
-@pytest.fixture
-def dummy_dataset() -> DataFrame:
-    """
-    Training dataset
-    """
-    size = 50
-    recipe = LogitRecipe(lambda variables, _: 0 + 10 * variables["a"])
-    recipe.add_variable(ContinousVariable("a"))
-    return recipe.cook(size)
-
-def test_training(dummy_dataset: DataFrame) -> None:
-    agent = PPOAgent(training_envs_count=2)
-    agent.train(steps=int(1e2), save_freq=1e2)
-    agent.analyze(dummy_dataset)
+def test_training() -> None:
+    agent = PPOAgent(training_envs_count=2,
+                     environment_kwargs={'data_generators': [datacooker_generator]})
+    agent.train(steps=50, save_freq=50)
+    agent.analyze(*datacooker_generator())

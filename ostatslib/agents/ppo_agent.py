9ebecb2731bba24fb23fbb24d3adcc3062e7df85
gui-lisboa <47819834+gui-lisboa@users.noreply.github.com>
2023-06-09 00:27:34 +0000

    feat: add environment kwargs to PPO init

--- ostatslib/agents/ppo_agent.py
+++ ostatslib/agents/ppo_agent.py
@@ -20,21 +20,27 @@ POLICY_KWARGS = {
 
 TRAINING_LOGS_PATH = "./.logs/"
 
+
 class PPOAgent(Agent):
     """
     Agent built on PPO algorithm model
     """
 
-    def __init__(self, path: str | None = None, training_envs_count: int = 10) -> None:
+    def __init__(self,
+                 path: str | None = None,
+                 training_envs_count: int = 1,
+                 environment_kwargs: dict | None = None) -> None:
+        self.__environment_kwargs = environment_kwargs
         self.__model = self.__init__model(path, training_envs_count)
 
-    def train(self, steps: int = 100000, save_freq: int = 1) -> None:
+    def train(self, steps: int = 1000000, save_freq: int = 100000) -> None:
         n_envs = self.__model.n_envs if self.__model.n_envs is not None else 1
         save_freq = max(save_freq // n_envs, 1)
         checkpoint_callback = CheckpointCallback(save_freq=save_freq,
                                                  save_path=TRAINING_LOGS_PATH,
                                                  save_replay_buffer=True)
-        logger = configure(TRAINING_LOGS_PATH, ["stdout", "csv", "tensorboard"])
+        logger = configure(TRAINING_LOGS_PATH,
+                           ["stdout", "csv", "tensorboard"])
         self.__model.set_logger(logger)
         self.__model.learn(total_timesteps=steps, callback=checkpoint_callback)
 
@@ -47,7 +53,13 @@ class PPOAgent(Agent):
 
     def __init__model(self, path: str | None, training_envs_count: int) -> PPO:
         if path is None:
-            environments = make_vec_env(GymEnvironment, training_envs_count)
-            return PPO(POLICY, environments, verbose=1, n_steps=6144, policy_kwargs=POLICY_KWARGS)
+            environments = make_vec_env(GymEnvironment,
+                                        training_envs_count,
+                                        env_kwargs=self.__environment_kwargs)
+            return PPO(POLICY,
+                       environments,
+                       verbose=1,
+                       n_steps=6144,
+                       policy_kwargs=POLICY_KWARGS)
 
         return PPO.load(path)

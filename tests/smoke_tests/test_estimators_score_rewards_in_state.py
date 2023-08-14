770c82c6c21b3577d7b39f1375b4c9d85e9219dd
Guilherme <g.lisboa.oliveira@outlook.com>
2023-08-14 19:16:30 +0000

    test: adds unmatched state score keys lookup test

--- tests/smoke_tests/test_estimators_score_rewards_in_state.py
+++ tests/smoke_tests/test_estimators_score_rewards_in_state.py
@@ -18,3 +18,27 @@ def test_estimators_score_rewards_in_state(module_) -> None:
     state = State()
     for _, action in actions:
         assert state.get(action.action_key + "_score_reward") == 0
+
+
+def test_no_unmatched_score_rewards_in_state() -> None:
+    modules = [classifiers, regressors]
+    actions: list[tuple[str, Action]] = []
+    for module_ in modules:
+        actions.extend(inspect.getmembers(module_, _is_action))
+
+    state_score_reward_keys = [
+        key for key in State().keys if key.endswith('_score_reward')
+    ]
+
+    unmatched_keys = []
+    for key in state_score_reward_keys:
+        found_match = False
+        for _, action in actions:
+            if key == action.action_key + "_score_reward":
+                found_match = True
+                break
+
+        if not found_match:
+            unmatched_keys.append(key)
+
+    assert not unmatched_keys

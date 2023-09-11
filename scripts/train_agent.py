3cb65bec57b64f1ed06414c31ea00be2ca9593d3
Guilherme <g.lisboa.oliveira@outlook.com>
2023-09-11 18:11:34 +0000

    feat: adds pre-trained model option to agent script

--- scripts/train_agent.py
+++ scripts/train_agent.py
@@ -15,12 +15,19 @@ if __name__ == '__main__':
                         type=int, default=int(500e3))
     parser.add_argument("--save-freq", nargs='?', const=1,
                         type=int, default=int(25e3))
-    parser.add_argument("--env-count", nargs='?', const=1, type=int, default=10)
+    parser.add_argument("--env-count", nargs='?',
+                        const=1, type=int, default=10)
     parser.add_argument("--name", nargs='?', const=1,
                         type=str, default=f'ppo_agent{datetime.now()}')
+    parser.add_argument("--model-path", nargs='?', const=1,
+                        type=str, default=None)
 
     args = parser.parse_args()
 
-    agent = PPOAgent(training_envs_count=int(args.env_count))
+    if args.model_path is None:
+        agent = PPOAgent(training_envs_count=int(args.env_count))
+    else:
+        agent = PPOAgent(args.model_path)
+
     agent.train(int(args.steps), int(args.save_freq))
     agent.save(args.name)
